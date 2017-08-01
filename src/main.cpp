8b3cb61b9e9058d6ec703467a63d5e15bf61c578
Trim Bresilla <trim.bresilla@gmail.com>
2017-08-01 16:07:09 +0200

    fix: methodised_v2

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,43 @@
 #include <Adafruit_SleepyDog.h>
 #include <WiFi101.h>
 
-
-
+String readSensor(int PINS){
+  String dataString = "";
+  for (int analogPin = 0; analogPin < PINS; analogPin++) {
+    int sensor = analogRead(analogPin);
+    int mapper = map(sensor, 0, 770, 1, 13000);
+    if (mapper > 13500){
+      mapper = 0;
+    }
+    dataString += String(mapper);
+    if (analogPin < PINS-1) {
+      dataString += ",";
+    }
+  }
+  return dataString;
+}
+void writeFILE(String dataString) {
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+  }
+  else {
+    Serial.println("Reading file failed!");
+  }
+}
+void writeSERIAL(String dataString) {
+	Serial.println(dataString);
+}
+void sleepTime(float minutes, bool Dog){
+  int millisec = minutes * 60000;
+  if (Dog){
+    Watchdog.sleep(millisec);
+  }
+  else{
+    delay(millisec);
+  }
+}
 void initSerial(int PT) {
   Serial.begin(PT);
   while (!Serial) {
@@ -40,28 +75,8 @@ void setup() {
 }
 
 void loop() {
-  String dataString = "";
-  for (int analogPin = 0; analogPin < 6; analogPin++) {
-    int sensor = analogRead(analogPin);
-    int mapper = map(sensor, 0, 770, 1, 13000);
-    if (mapper > 13500){
-      mapper = 0;
-    }
-    dataString += String(mapper);
-    if (analogPin < 5) {
-      dataString += ",";
-    }
-  }
-
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
-  if (dataFile) {
-    dataFile.println(dataString);
-    dataFile.close();
-    Serial.println(dataString);
-  }
-  else {
-    Serial.println("error opening datalog.txt");
-  }
-  delay(5000);
-  //Watchdog.sleep(20000);
+  String data = readSensor(6);
+  writeFILE(data);
+  writeSERIAL(data);
+  sleepTime(0.1, false);
 }
